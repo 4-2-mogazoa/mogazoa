fdcf72d1997cd85a6aaf810584cbe91318051ef9
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-03-09 19:05:20 +0900

    refactor: 각 타입별 데이터 객체형태로 정리, 함수 분리
    최대한 하드 코딩했던 부분들을 간결하게 정리하려고 했는데, 오히려 더 길어진 느낌이네요. ㅎㅎ; 그래도 이전보다 jsx문법에서의 불필요한 조건부 렌더링은 줄인거같습니다.

--- src/components/common/statisticscard/StatisticsCard.tsx
+++ src/components/common/statisticscard/StatisticsCard.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import calculateDifference from "./calculateDifference";
+
 type StatisticsCardProps = {
 	type: "rate" | "like" | "review";
 	rateData?: number;
@@ -19,128 +21,84 @@ export default function StatisticsCard({
 	likeAvg,
 	reviewAvg,
 }: StatisticsCardProps) {
-	const typeLabel =
-		type === "rate" ? "별점 평균" : type === "like" ? "찜" : "리뷰";
-	const typeIcon =
-		type === "rate"
-			? "/icons/star.svg"
-			: type === "like"
-				? "/icons/heart_on.svg"
-				: "/icons/message.svg";
-	const reviewComma = reviewData?.toLocaleString("ko-KR");
-	const data =
-		type === "rate"
-			? rateData || 0
-			: type === "like"
-				? likeData || 0
-				: reviewData || 0;
-	const avgData =
-		type === "rate"
-			? rateAvg || 0
-			: type === "like"
-				? likeAvg || 0
-				: reviewAvg || 0;
-	function calculateDifference(data: number, avgData: number) {
-		const isBigger = data > avgData;
-		const isSame = data === avgData;
-		const diffRate = (isBigger ? data - avgData : avgData - data).toFixed(1);
-		const diffLikeNReview = Math.ceil(
-			isBigger ? data - avgData : avgData - data,
-		);
-		const biggerRate = isBigger && type === "rate";
-		const biggerLikeNReview =
-			(isBigger && type === "like") || (isBigger && type === "review");
-		const comparisonRate = biggerRate ? "더 높아요!" : "더 낮아요!";
-		const comparisonLikeNReview = biggerLikeNReview
-			? "더 많아요!"
-			: "더 적어요!";
-		return {
-			isSame,
-			diffRate,
-			diffLikeNReview,
-			comparisonRate,
-			comparisonLikeNReview,
-		};
-	}
-	const dataDiff = calculateDifference(data, avgData);
+	const typeList = {
+		rate: {
+			label: "별점 평균",
+			productData: rateData,
+			avgData: rateAvg,
+			icon: "/icons/star.svg",
+			unit: "점",
+			bottomDecription: {
+				higher: "더 높아요!",
+				lower: "더 낮아요!",
+				same: "동일해요!",
+			},
+		},
+		like: {
+			label: "찜",
+			productData: likeData,
+			avgData: likeAvg,
+			icon: "/icons/heart_on.svg",
+			unit: "개",
+			bottomDecription: {
+				higher: "더 많아요!",
+				lower: "더 적어요!",
+				same: "동일해요!",
+			},
+		},
+		review: {
+			label: "리뷰",
+			productData: reviewData,
+			avgData: reviewAvg,
+			icon: "/icons/message.svg",
+			unit: "개",
+			bottomDecription: {
+				higher: "더 많아요!",
+				lower: "더 적어요!",
+				same: "동일해요!",
+			},
+		},
+	};
 
+	const { label, productData, avgData, icon, unit, bottomDecription } =
+		typeList[type];
+
+	const result = calculateDifference(productData, avgData, bottomDecription);
 	return (
 		<div className="flex h-[8.2rem] min-w-[33.5rem] flex-col justify-center rounded-[1.2rem] border border-[#353542] bg-[#252530] px-[2rem] md:min-h-[16.9rem] md:min-w-[21.8rem] md:items-center md:gap-[2rem] lg:h-[19rem] lg:w-[30rem] lg:items-center lg:gap-[2rem]">
 			<div className="flex flex-row gap-[1rem]">
 				<span className="text-[1.4rem] text-white md:text-[1.6rem] lg:text-[1.8rem]">
-					{typeLabel}
+					{label}
 				</span>
 				<div className="flex items-center md:hidden lg:hidden">
 					<div className="relative mr-[0.5rem] size-[1.9rem]  md:size-[2rem] lg:size-[2.4rem]">
-						<Image
-							src={typeIcon}
-							alt={typeLabel}
-							fill
-							className="object-cover"
-						/>
+						<Image src={icon} alt={label} fill className="object-cover" />
 					</div>
-
 					<span className="text-[1.6rem] text-gray-100 md:text-[2rem] lg:text-[2.4rem]">
-						{rateData}
-					</span>
-					<span className="text-[1.6rem] text-gray-100 md:text-[2rem] lg:text-[2.4rem] ">
-						{likeData}
-					</span>
-					<span className="text-[1.6rem] text-gray-100 md:text-[2rem] lg:text-[2.4rem] ">
-						{reviewComma}
+						{type === "rate" ? productData : productData?.toLocaleString()}
 					</span>
 				</div>
 			</div>
 			<div className="hidden md:flex md:items-center lg:flex lg:items-center">
 				<div className="relative mr-[0.5rem] size-[1.9rem]  md:size-[2rem] lg:size-[2.4rem]">
-					<Image
-						src={typeIcon}
-						alt={typeLabel}
-						fill
-						className="object-cover "
-					/>
+					<Image src={icon} alt={label} fill className="object-cover " />
 				</div>
 				<span className="text-[1.6rem] text-gray-100 md:text-[2rem] lg:text-[2.4rem]">
-					{rateData}
-				</span>
-				<span className="text-[1.6rem] text-gray-100 md:text-[2rem] lg:text-[2.4rem] ">
-					{likeData}
-				</span>
-				<span className="text-[1.6rem] text-gray-100 md:text-[2rem] lg:text-[2.4rem] ">
-					{reviewComma}
+					{type === "rate" ? productData : productData?.toLocaleString()}
 				</span>
 			</div>
-			{!dataDiff.isSame && (
-				<div className="flex flex-row items-center text-[1.2rem] md:flex-col lg:flex-col lg:text-[1.4rem]">
-					<span className="text-gray-200">
-						같은 카테고리의 제품들보다&nbsp;
-					</span>
-					{rateData && (
-						<span className="text-gray-200">
-							<span className="text-white">{dataDiff.diffRate}점 </span>
-							{dataDiff.comparisonRate}
+			<div className="flex flex-row items-center text-[1.2rem] md:flex-col lg:flex-col lg:text-[1.4rem]">
+				<span className="text-gray-200">{result.topDescription}&nbsp;</span>
+				<span className="text-gray-200">
+					{!result.isSame && (
+						<span className="text-white">
+							{type === "rate" ? result.rateDiff : result.likeReviewDiff}
+							{unit}&nbsp;
 						</span>
 					)}
-					{likeData && (
-						<span className="text-gray-200">
-							<span className="text-white">{dataDiff.diffLikeNReview}개 </span>
-							{dataDiff.comparisonLikeNReview}
-						</span>
-					)}
-					{reviewData && (
-						<span className="text-gray-200">
-							<span className="text-white">{dataDiff.diffLikeNReview}개 </span>
-							{dataDiff.comparisonLikeNReview}
-						</span>
-					)}
-				</div>
-			)}
-			{dataDiff.isSame && (
-				<div className="flex flex-row items-center text-[1.2rem] text-gray-200 md:flex-col lg:flex-col lg:text-[1.4rem]">
-					<span>같은 카테고리의 제품들과&nbsp;</span>
-					<span>동일해요!</span>
-				</div>
-			)}
+					{result.bottomDescriptionResult}
+				</span>
+			</div>
 		</div>
 	);
 }
