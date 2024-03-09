68c9597a7ab2cb6b092e97c3457a7c9a2b25fdc3
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-03-09 19:58:55 +0900

    refactor: 불필요한 조건부 렌더링 제거, clsx 사용
    - 사이즈별 label을 구분하지 않고, 하나의 label로 구현하도록 수정
    - 조건부 렌더링부분을 제거하고 clsx를 사용해서 type이 review일 경우에만 패딩값이 들어오도록 수정

--- src/components/common/activitycard/ActivityCard.tsx
+++ src/components/common/activitycard/ActivityCard.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import Image from "next/image";
 
 import CategoryBadge from "../categoryBadge/CategoryBadge";
@@ -19,61 +20,66 @@ type Props = {
 		| "앱";
 };
 
+type dataType = {
+	label: string[];
+	img?: string;
+	data: number | string | undefined;
+};
+
+type DataListType = {
+	rate: dataType;
+	review: dataType;
+	category: dataType;
+};
+
 export default function ActivityCard({
 	type,
 	myRateAvg,
 	myReivewCount,
 	category = "음악",
 }: Props) {
-	const typeList = {
+	const DataList: DataListType = {
 		rate: {
-			label: "남긴 별점 평균",
-			mobileLabel: ["남긴", "별점 평균"],
+			label: ["남긴", "별점 평균"],
 			img: "/icons/star.svg",
 			data: myRateAvg?.toFixed(1),
-			isReview: false,
 		},
 		review: {
-			label: "남긴 리뷰",
-			mobileLabel: ["남긴 리뷰"],
+			label: ["남긴 리뷰"],
 			img: "/icons/message.svg",
 			data: myReivewCount?.toLocaleString(),
-			isReview: true,
 		},
 		category: {
-			label: "관심 카테고리",
-			mobileLabel: ["관심", "카테고리"],
-			img: "",
+			label: ["관심", "카테고리"],
 			data: category,
-			isReview: false,
 		},
 	};
-
-	const { label, img, mobileLabel, data, isReview } = typeList[type];
+	const { label, img, data } = DataList[type];
 
 	return (
 		<div className="flex h-[11.9rem] w-[10.5rem] flex-col items-center justify-center gap-[1.5rem] rounded-[1.2rem] border border-[#353542] bg-[#252530] md:w-[16.3rem] lg:h-[12.8rem] lg:w-[30rem] lg:gap-[2rem]">
-			<div className="hidden text-[1.4rem] text-gray-100 md:flex lg:flex lg:text-[1.6rem]">
-				{label}
+			<div className="hidden text-[1.4rem] text-gray-100 md:flex lg:text-[1.6rem]">
+				{label.join(" ")}
 			</div>
-			{!isReview && (
-				<div className="flex text-center text-[1.4rem] text-gray-100 md:hidden lg:hidden lg:text-[1.6rem]">
-					{mobileLabel[0]}
-					<br />
-					{mobileLabel[1]}
-				</div>
-			)}
-			{isReview && (
-				<div className="flex py-[1rem] text-center text-[1.4rem] text-gray-100 md:hidden lg:hidden lg:text-[1.6rem]">
-					{mobileLabel[0]}
-					<br />
-					{mobileLabel[1]}
-				</div>
-			)}
+			<div
+				className={clsx(
+					"flex text-center text-[1.4rem] text-gray-100 md:hidden lg:text-[1.6rem]",
+					{ "py-[1rem]": type === "review" },
+				)}
+			>
+				{label[0]}
+				<br />
+				{label[1]}
+			</div>
 			{img && (
 				<div className="flex flex-row items-center ">
 					<div className="relative mr-[0.5rem] size-[2rem] lg:size-[2.4rem]">
-						<Image src={img} alt={label} fill className="object-cover" />
+						<Image
+							src={img}
+							alt={label.join("")}
+							fill
+							className="object-cover"
+						/>
 					</div>
 					<span className="text-[2rem] text-white lg:text-[2.4rem]">
 						{data}
