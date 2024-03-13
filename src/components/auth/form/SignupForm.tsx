e73242a84ac9aae58cd8948428abd2318849db97
김우현 <rladngus133@naver.com>
2024-03-13 16:29:06 +0900

    feat: 회원가입 정규식 패턴 및 검증 적용

--- src/components/auth/form/SignupForm.tsx
+++ src/components/auth/form/SignupForm.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 
 import BasicButton from "@/components/common/button/BasicButton";
 import Input from "@/components/common/inputs/Input";
+import { emailPattern, passwordPattern } from "@/constants/regExp";
 
 import AuthFormContainer from "./AuthFormContainer";
 
@@ -18,22 +19,45 @@ export default function SignupForm() {
 		register,
 		handleSubmit,
 		formState: { errors },
+		getValues,
 	} = useForm<ISignupForm>({ mode: "onBlur" });
 
 	const emailValidationSchema = {
 		required: "이메일은 필수 입력입니다.",
+		pattern: {
+			value: emailPattern,
+			message: "이메일 형식으로 작성해 주세요.",
+		},
 	};
 
 	const nicknameValidationSchema = {
 		required: "닉네임은 필수 입력입니다.",
+		maxLength: {
+			value: 20,
+			message: "닉네임은 최대 20자까지 가능합니다.",
+		},
 	};
 
 	const passwordValidationSchema = {
 		required: "비밀번호는 필수 입력입니다.",
+		pattern: {
+			value: passwordPattern,
+			message: "비밀번호는 숫자, 영문, 특수문자로만 가능합니다.",
+		},
+		minLength: {
+			value: 8,
+			message: "비밀번호는 최소 8자 이상입니다.",
+		},
 	};
 
 	const passwordCkdValidationSchema = {
-		required: "비밀번호확인은 필수 입력입니다.",
+		required: "비밀번호 확인을 입력해주세요.",
+		validate: {
+			matchesPreviousPassword: (value: string) => {
+				const { password } = getValues();
+				return password === value || "비밀번호가 일치하지 않습니다.";
+			},
+		},
 	};
 
 	return (
