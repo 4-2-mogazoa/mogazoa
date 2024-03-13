9520d8a7b63fb09317345ff139409f8ee80e710c
김우현 <rladngus133@naver.com>
2024-03-13 10:25:06 +0900

    feat: SingInForm 작성(설명참조)
    AuthFormContainer 로그인,회원가입,Oauth등 폼사용시 전역적으로
    사용할수있는 공통폼 컴포넌트
    
    SingInForm 로그인 인풋 정보를 담고있는 컴포넌트
    
    Input.tsx에 React-hook-form의 각종정보들 추가
    
    정규식은 constatns의 RegExp에서 관리함

--- src/components/common/auth/form/AuthFormContainer.tsx
+++ src/components/common/auth/form/AuthFormContainer.tsx
@@ -1,11 +1,19 @@
 import React, { ReactNode } from "react";
+import { FieldValues, SubmitHandler } from "react-hook-form";
 
-export default function AuthFormContainer({
-	children,
-}: {
-	children: ReactNode;
-}) {
+type Props = { children: ReactNode; handleSubmit: any };
+
+export default function AuthFormContainer({ children, handleSubmit }: Props) {
+	const onSubmit: SubmitHandler<FieldValues> = (data) => {
+		console.log("데이터 :", data);
+	};
+
 	return (
-		<form className="w-full max-w-[44rem] lg:max-w-[64rem]">{children}</form>
+		<form
+			className="w-full max-w-[44rem] lg:max-w-[64rem]"
+			onSubmit={handleSubmit(onSubmit)}
+		>
+			{children}
+		</form>
 	);
 }
--- src/components/common/auth/form/SignInForm.tsx
+++ src/components/common/auth/form/SignInForm.tsx
@@ -1,13 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
+import { emailPattern } from "@/constants/regExp";
+
+import BasicButton from "../../button/BasicButton";
 import Input from "../../inputs/Input";
 import AuthFormContainer from "./AuthFormContainer";
 
+export interface ISignInForm {
+	email: string;
+	password: string;
+}
+
 export default function SignInForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<ISignInForm>();
+
+	const emailValidationSchema = {
+		required: "이메일은 필수 입력입니다.",
+		pattern: {
+			value: emailPattern,
+			message: "이메일 형식으로 작성해 주세요.",
+		},
+	};
+
+	const passwordValidationSchema = {
+		required: "비밀번호는 필수 입력입니다.",
+	};
+
 	return (
-		<AuthFormContainer>
-			<Input inputType="email" />
-			<Input inputType="password" />
+		<AuthFormContainer handleSubmit={handleSubmit}>
+			<div className="mb-[6rem] flex flex-col gap-[4rem]">
+				<Input
+					inputType="email"
+					register={register}
+					validationSchema={emailValidationSchema}
+					errors={errors}
+				/>
+				<Input
+					inputType="password"
+					register={register}
+					validationSchema={passwordValidationSchema}
+					errors={errors}
+				/>
+			</div>
+			<BasicButton label="로그인" />
 		</AuthFormContainer>
 	);
 }
