736d521ca5aa89dd22f30429021c34e620fc7223
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-03-14 19:40:45 +0900

    feat: 상품 상세 데이터 타입 작성

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,3 +4,32 @@ export type Category = {
 	name: string;
 	id: number;
 };
+
+export type ProductDetail = {
+	id: number;
+	name: string;
+	description: string;
+	image: string;
+	rating: number;
+	reviewCount: number;
+	favoriteCount: number;
+	categoryId: number;
+	createdAt: string;
+	updatedAt: string;
+	writerId: number;
+	isFavorite: boolean;
+	category: {
+		id: number;
+		name:
+			| "음악"
+			| "영화/드라마"
+			| "강의/책"
+			| "호텔"
+			| "가구/인테리어"
+			| "식당"
+			| "전자기기"
+			| "화장품"
+			| "의류/잡화"
+			| "앱";
+	};
+};
