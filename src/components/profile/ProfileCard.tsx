6a1c22d9062ade8cb95f4b30525c737ee5af6433
ckswnskfk <ckswnskfk@hanmail.net>
2024-03-15 01:04:28 +0900

    style: 프로필 카드 구현

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,7 +1,6 @@
+import BasicButton from "@/components/common/button/BasicButton";
+import ProfileImage from "@/components/common/profileImage/ProfileImage";
 import { UserDetail } from "@/types/user";
-
-import BasicButton from "../common/button/BasicButton";
-import ProfileImage from "../common/profileImage/ProfileImage";
 
 type Props = {
 	user: UserDetail;
@@ -10,16 +9,17 @@ type Props = {
 
 export default function ProfileCard({ user, isMine = true }: Props) {
 	return (
-		<div className="_flex-col-center w-[33.5rem] gap-[3rem] rounded-[1.2rem] border border-black-border bg-black-bg px-[2rem] py-[3rem] md:w-[50.9rem] lg:w-[34rem]">
+		<section className="_flex-col-center w-[33.5rem] gap-[3rem] rounded-[1.2rem] border border-black-border bg-black-bg px-[2rem] py-[3rem] md:w-[50.9rem] lg:w-[34rem]">
+			<h2 className="sr-only">기본 정보</h2>
 			<ProfileImage
 				size="large"
 				src={"https://github.com/pmndrs/zustand/raw/main/bear.jpg"}
 			/>
 			<div className="flex w-full flex-col gap-[1rem]">
-				<h1 className="self-center text-[2rem] font-semibold text-white">
+				<strong className="self-center text-[2rem] font-semibold text-white">
 					{user.nickname}
-				</h1>
-				<p className="max-w-[29.5rem] text-[1.4rem] text-gray-200">
+				</strong>
+				<p className="text-[1.4rem] text-gray-200">
 					세상에 리뷰 못할 제품은 없다. surisuri마수리와 함께라면 당신도
 					프로쇼핑러! 안녕하세요, 별점의 화신 surisuri마수리입니다!
 				</p>
@@ -40,7 +40,7 @@ export default function ProfileCard({ user, isMine = true }: Props) {
 				</div>
 			</div>
 			{isMine ? (
-				<div className="_flex-col-center w-full gap-[1rem]">
+				<div className="_flex-col-center w-full gap-[1rem] md:gap-[1.5rem] lg:gap-[2rem]">
 					<BasicButton variant={"primary"} label="프로필 편집" />
 					<BasicButton variant={"tertiary"} label="로그아웃" />
 				</div>
@@ -50,6 +50,6 @@ export default function ProfileCard({ user, isMine = true }: Props) {
 					variant={user.isFollowing ? "tertiary" : "primary"}
 				/>
 			)}
-		</div>
+		</section>
 	);
 }
