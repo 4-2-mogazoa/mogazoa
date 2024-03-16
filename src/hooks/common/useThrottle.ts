9bc64ec85aba8bdab22d302b051e61346b3ee02f
ckswnskfk <ckswnskfk@hanmail.net>
2024-03-16 17:15:37 +0900

    refactor: 메모리 누수 방지를 위해 명시적으로 timeout 정리

--- src/hooks/common/useThrottle.ts
+++ src/hooks/common/useThrottle.ts
@@ -1,23 +1,32 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 function useThrottle<T extends (...args: any[]) => void>(
 	callback: T,
 	delay: number,
 ): T {
 	const isThrottling = useRef(false);
+	const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
 
 	const throttledCallback = useCallback(
 		(...args: any[]) => {
 			if (!isThrottling.current) {
 				callback(...args);
 				isThrottling.current = true;
-				setTimeout(() => {
+				timer.current = setTimeout(() => {
 					isThrottling.current = false;
 				}, delay);
 			}
 		},
 		[callback, delay],
 	);
+
+	useEffect(() => {
+		return () => {
+			if (timer.current) {
+				clearTimeout(timer.current);
+			}
+		};
+	}, []);
 
 	return throttledCallback as T;
 }
