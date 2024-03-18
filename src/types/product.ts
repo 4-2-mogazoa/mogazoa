be1948e873974403cfec969e0c9d3491d3bc31b7
Chyyni <catharina0409@gmail.com>
2024-03-18 16:53:30 +0900

    feat: 상품 목록 조회 api 타입 지정

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,3 +1,5 @@
+import { CategoryList, Response } from "./common";
+
 export type ProductDetail = {
 	id: number;
 	name: string;
@@ -13,16 +15,21 @@ export type ProductDetail = {
 	isFavorite: boolean;
 	category: {
 		id: number;
-		name:
-			| "음악"
-			| "영화/드라마"
-			| "강의/책"
-			| "호텔"
-			| "가구/인테리어"
-			| "식당"
-			| "전자기기"
-			| "화장품"
-			| "의류/잡화"
-			| "앱";
+		name: CategoryList;
 	};
 };
+
+export type Product = {
+	id: number;
+	name: string;
+	image: string;
+	rating: number;
+	reviewCount: number;
+	favoriteCount: number;
+	categoryId: number;
+	createdAt: string;
+	updatedAt: string;
+	writerId: number;
+};
+
+export type ProductsResponse = Response<Product>;
