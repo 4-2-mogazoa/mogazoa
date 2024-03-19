e85a770422ae7c20615906a54348eb081f0245b9
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-03-19 18:40:05 +0900

    feat: ProductDetailPageLayout 컴포넌트 구현

---
+++ src/components/productdetail/ProductDetailPageLayout.tsx
@@ -0,0 +1,13 @@
+import ProductDetail from "./ProductDetail";
+import ProductReview from "./ProductReview";
+import ProductStatistics from "./ProductStatistics";
+
+export default function ProductDetailPageLayout() {
+	return (
+		<main className="_flex-col-center gap-[6rem] bg-[#1C1C22] px-[2rem] py-[3rem] md:px-[3rem] md:py-[4rem] lg:py-[6rem]">
+			<ProductDetail />
+			<ProductStatistics />
+			<ProductReview />
+		</main>
+	);
+}
