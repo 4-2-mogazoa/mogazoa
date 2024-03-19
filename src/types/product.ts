5e62a61886ad04ab9b563393ff4fa2f49e93889d
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-03-20 01:00:02 +0900

    chore: 타입 수정

--- src/types/product.ts
+++ src/types/product.ts
@@ -27,6 +27,11 @@ export type ProductDetail = {
 			| "의류/잡화"
 			| "앱";
 	};
+	categoryMetric: {
+		rating: number;
+		favoriteCount: number;
+		reviewCount: number;
+	};
 };
 
 export type ReviewResponse = Response<Review>;
