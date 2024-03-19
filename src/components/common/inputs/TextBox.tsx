914ae7efce0e4cca6ea46d4a6d3aeb1272bce66c
ckswnskfk <ckswnskfk@hanmail.net>
2024-03-19 22:35:59 +0900

    feat: TextBox 외부에서 props 받을 수 있게 변경

--- src/components/common/inputs/TextBox.tsx
+++ src/components/common/inputs/TextBox.tsx
@@ -1,10 +1,18 @@
-import React, { ChangeEvent, useState } from "react";
+import React, {
+	ButtonHTMLAttributes,
+	ChangeEvent,
+	TextareaHTMLAttributes,
+	useState,
+} from "react";
 
-export default function TextBox() {
+export default function TextBox({
+	maxLength,
+	...props
+}: TextareaHTMLAttributes<HTMLTextAreaElement>) {
 	const [count, setCount] = useState(0);
 	const [isFocused, setIsFocused] = useState(false);
 
-	const hadleOnTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+	const handleOnTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
 		setCount(e.target.value.length);
 	};
 
@@ -13,18 +21,18 @@ export default function TextBox() {
 			className={`min-h-[8.8rem] rounded-xl border ${isFocused ? "border-main_blue" : "border-[#353542]"} bg-[#252530] p-[2rem]`}
 		>
 			<textarea
-				maxLength={300}
-				className="w-full resize-none overflow-hidden border-none bg-[#252530] text-[1.4rem] text-white placeholder:text-[1.4rem] focus:outline-none lg:text-[1.6rem] lg:placeholder:text-[1.6rem]"
-				placeholder="리뷰를 작성해 주세요"
+				className="w-full resize-none overflow-hidden border-none bg-[#252530] text-[1.4rem] text-white placeholder:text-[1.4rem] placeholder:text-gray-200 focus:outline-none lg:text-[1.6rem] lg:placeholder:text-[1.6rem]"
 				rows={3}
-				onChange={hadleOnTextarea}
+				onChange={handleOnTextarea}
 				onFocus={() => setIsFocused(true)}
 				onBlur={() => setIsFocused(false)}
+				maxLength={maxLength}
+				{...props}
 			/>
 
 			<p className="text-right text-[1.4rem] text-[#6E6E82]">
 				<span>{count}</span>
-				<span>/300 자</span>
+				<span>/{maxLength}</span>
 			</p>
 		</div>
 	);
