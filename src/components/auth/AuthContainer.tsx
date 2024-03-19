5a3a72d00ddc9e3721d2e0c24df380bd2332d873
김우현 <rladngus133@naver.com>
2024-03-20 06:28:23 +0900

    fix: app.tsx 헤더 제거 , Auth페이지 헤더 추가

--- src/components/auth/AuthContainer.tsx
+++ src/components/auth/AuthContainer.tsx
@@ -1,9 +1,20 @@
 import React, { ReactNode } from "react";
 
+import Header from "../common/menu/Header";
+
 export default function AuthContainer({ children }: { children: ReactNode }) {
+	const scrollbarStyles = {
+		WebkitScrollbar: {
+			display: "none",
+		},
+		msOverflowStyle: "none",
+		scrollbarWidth: "none",
+	};
+
 	return (
 		<div className="w-full bg-[#1c1c22]">
-			<div className="flex items-center justify-center px-[2rem]">
+			<Header />
+			<div className="flex h-screen items-center justify-center overflow-auto p-[2rem]">
 				{children}
 			</div>
 		</div>
