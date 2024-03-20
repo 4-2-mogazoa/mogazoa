1e47d126ec948dcf3a166853a72bd0d2dc22ff11
ckswnskfk <ckswnskfk@hanmail.net>
2024-03-20 16:12:39 +0900

    feat: 팔로워 모달을 위해 버튼 태그로 변경, 핸들러 함수명 변경

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -13,7 +13,7 @@ type Props = {
 export default function ProfileCard({ user, isMine = true }: Props) {
 	const { openModal, closeModal } = useModalActions();
 
-	const handleOpenModal = () => {
+	const handleOpenProfileModifyModal = () => {
 		const modalId = openModal(
 			<ProfileModifyModal user={user} closeModal={() => closeModal(modalId)} />,
 		);
@@ -33,26 +33,26 @@ export default function ProfileCard({ user, isMine = true }: Props) {
 				<p className="text-[1.4rem] text-gray-200">{user.description}</p>
 			</div>
 			<div className="flex w-full justify-evenly">
-				<div className="_flex-col-center">
-					<p className="text-[1.8rem] font-semibold text-white">
+				<button className="_flex-col-center">
+					<span className="text-[1.8rem] font-semibold text-white">
 						{user.followeesCount}
-					</p>
-					<p className="text-[1.4rem] text-gray-100">팔로워</p>
-				</div>
+					</span>
+					<span className="text-[1.4rem] text-gray-100">팔로워</span>
+				</button>
 				<div className="h-[4.8rem] w-[1px] bg-black-border"></div>
-				<div className="_flex-col-center">
-					<p className="text-[1.8rem] font-semibold text-white">
+				<button className="_flex-col-center">
+					<span className="text-[1.8rem] font-semibold text-white">
 						{user.followersCount}
-					</p>
-					<p className="text-[1.4rem] text-gray-100">팔로잉</p>
-				</div>
+					</span>
+					<span className="text-[1.4rem] text-gray-100">팔로잉</span>
+				</button>
 			</div>
 			{isMine ? (
 				<div className="_flex-col-center w-full gap-[1rem] md:gap-[1.5rem] lg:gap-[2rem]">
 					<BasicButton
 						variant={"primary"}
 						label="프로필 편집"
-						onClick={handleOpenModal}
+						onClick={handleOpenProfileModifyModal}
 					/>
 					<BasicButton variant={"tertiary"} label="로그아웃" />
 				</div>
