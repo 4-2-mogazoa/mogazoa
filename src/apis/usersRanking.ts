179da1118ed8cdb53fd38569178e6c77bad4c4af
d0ngbb00 <lauren3021504@gmail.com>
2024-03-23 20:54:13 +0900

    fix: 리뷰어 랭킹 정렬 기준 변경

--- src/apis/usersRanking.ts
+++ src/apis/usersRanking.ts
@@ -6,14 +6,7 @@ export const getUsersRanking = async() => {
   try {
     const response = await instance.get("/users/ranking");
     const data = response.data;
-    const sortedData = data.sort((a: UsersRanking, b: UsersRanking) => {
-      if (b.reviewCount !== a.reviewCount) {
-        return b.reviewCount - a.reviewCount;
-      } else {
-        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-      }
-    });
-    const top5Data = sortedData.slice(0, 5).map((userData: UsersRanking, index: number) => ({
+    const top5Data = data.slice(0, 5).map((userData: UsersRanking, index: number) => ({
       ...userData,
       rank: index + 1
     }));
