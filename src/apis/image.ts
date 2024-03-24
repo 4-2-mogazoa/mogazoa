35e26c14d0ff182b5a3befd8fe897416dea525b0
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-03-25 05:45:39 +0900

    feat: 이미지 업로드 api 작성

---
+++ src/apis/image.ts
@@ -0,0 +1,23 @@
+import { ImageData } from "@/components/common/inputs/AddImageBox";
+
+import instance from "./axiosInstance";
+
+type image = {
+	url: string;
+};
+
+export async function getImageURL(file: File | undefined) {
+	const res = await instance.post<image>(
+		"images/upload",
+		{
+			image: file,
+		},
+		{
+			headers: {
+				"Content-Type": "multipart/form-data",
+			},
+		},
+	);
+
+	return res.data;
+}
