f2cba1fd0ba631d85dee6d7ffcc0a2b6e694fd20
Chyyni <catharina0409@gmail.com>
2024-03-27 23:47:30 +0900

    chore: useTrapFocus 함수에 JDOC 정의

--- src/hooks/common/useTrapFocus.ts
+++ src/hooks/common/useTrapFocus.ts
@@ -1,5 +1,9 @@
 import { useRef } from "react";
 
+/**
+ * 반환되는 focusableElements 배열에 tab으로 이동이 가능하게 하고 싶은 DOM 요소를 차례로 저장해주시면 됩니다.
+ * @returns focusableElements: tab을 이용하여 이동하고 싶은 DOM 요소를 담는 배열, handleModalTrapFocus: tab, shift+tab으로 이동할 때, 모달 영역을 벗어나지 않도록 하는 함수(modalWrapper에 적용)
+ */
 export default function useTrapFocus() {
 	const focusableElements = useRef<HTMLElement[]>([]);
 
