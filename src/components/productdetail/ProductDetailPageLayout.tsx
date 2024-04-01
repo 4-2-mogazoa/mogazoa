433eb84491ac15d70e6c128caf168ba16d757f72
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-04-02 01:22:53 +0900

    feat: 페이지 에러 처리

--- src/components/productdetail/ProductDetailPageLayout.tsx
+++ src/components/productdetail/ProductDetailPageLayout.tsx
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import { getProductDetail } from "@/apis/products";
 
 import AddProductButton from "../common/button/AddProductButton";
 import ProductDetail from "./ProductDetail";
@@ -8,6 +12,25 @@ import ProductStatistics from "./ProductStatistics";
 export default function ProductDetailPageLayout() {
 	const router = useRouter();
 	const productId = Number(router.query.id);
+
+	const { error } = useQuery({
+		queryKey: ["productDetail", productId],
+		queryFn: () => getProductDetail(productId),
+		enabled: !!productId,
+		staleTime: 60 * 1000,
+	});
+
+	useEffect(() => {
+		if (router.isReady && isNaN(productId)) {
+			router.push("/");
+		}
+
+		if (
+			(error as any)?.response?.data?.message === "상품을 찾을 수 없습니다."
+		) {
+			router.push("/");
+		}
+	}, [productId, router.isReady, error]);
 
 	return (
 		<main className="_flex-col-center gap-[6rem] bg-[#1C1C22] px-[2rem] py-[3rem] md:px-[3rem] md:py-[4rem] lg:py-[6rem]">
