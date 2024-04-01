9b5cd37f823aa3178197ef0d27c6b5119ea8cebd
ckswnskfk <ckswnskfk@hanmail.net>
2024-04-02 01:08:17 +0900

    feat: mypage 서버 사이드 렌더링 적용

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,17 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 
 import { getMe } from "@/apis/user";
 import ProfilePageLayout from "@/components/user/ProfilePageLayout";
-import { UserDetail } from "@/types/user";
+import getAccessTokenFromReq from "@/utils/getAccessTokenFromReq";
 
-export default function MyPage() {
-	const { data: me } = useQuery<UserDetail>({
-		queryKey: ["me"],
-		queryFn: getMe,
+export async function getServerSideProps({ req }: GetServerSidePropsContext) {
+	const accessToken = getAccessTokenFromReq(req);
+	if (!accessToken) {
+		return {
+			redirect: {
+				destination: "/signin",
+				permanent: false,
+			},
+		};
+	}
+
+	const me = await getMe({
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
 	});
 
-	if (!me) {
-		return <div>Loading...</div>;
-	}
+	return {
+		props: { me },
+	};
+}
+
+export default function MyPage({
+	me,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	return <ProfilePageLayout user={me} />;
 }
