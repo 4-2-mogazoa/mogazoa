1a0e445db3b1707d802364880667813a5013f077
김우현 <rladngus133@naver.com>
2024-04-03 20:03:29 +0900

    feat: oauth 페이지 레이아웃개선

--- src/pages/oauth/index.tsx
+++ src/pages/oauth/index.tsx
@@ -2,13 +2,12 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
 import { postOAuthSignin } from "@/apis/auth/postOAuthSignin";
+import LodingSpinner from "@/components/common/spinner/LodingSpinner";
 import getCookies from "@/utils/getCookies";
 
 export default function OAuth() {
 	const router = useRouter();
 	const { code } = router.query;
-
-	console.log(code);
 
 	useEffect(() => {
 		if (getCookies().accessToken) {
@@ -22,5 +21,9 @@ export default function OAuth() {
 		}
 	}, [code, router]);
 
-	return <div>Loding...</div>;
+	return (
+		<div className="flex h-screen w-full items-center justify-center bg-[#1c1c22]">
+			<LodingSpinner />
+		</div>
+	);
 }
